9d597edb1c2af1fe24104798751038acc69197e5
薛金峰 <3297039522@qq.com>
2023-01-13 08:48:09 +0800

    薛金峰-2023/1/13-商品统计

--- 项目/src/views/stat/goods.vue
+++ 项目/src/views/stat/goods.vue
@@ -1,3 +1,43 @@
 <template>
-  <div>商品统计</div>
+  <div class="app-container">
+    <ve-histogram
+      :extend="chartExtend"
+      :data="chartData"
+      :settings="chartSettings"
+    />
+  </div>
 </template>
+
+<script>
+import { statGoods } from "@/api/stat";
+import VeHistogram from "v-charts/lib/line";
+
+export default {
+  components: { VeHistogram },
+  data() {
+    return {
+      chartData: {},
+      chartSettings: {},
+      chartExtend: {}
+    };
+  },
+  created() {
+    statGoods().then(response => {
+      this.chartData = response.data.data;
+      this.chartSettings = {
+        labelMap: {
+          orders: "订单量",
+          products: "下单货品数量",
+          amount: "下单货品总额"
+        }
+      };
+      this.chartExtend = {
+        xAxis: { boundaryGap: true },
+        series: {
+          label: { position: "top" }
+        }
+      };
+    });
+  }
+};
+</script>
