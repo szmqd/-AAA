1e23cad9eb067ad8b5d1d2fe5d4e274360130b81
孙振美 <szm980331@163.com>
2023-01-13 11:42:07 +0800

    孙振美-1.13-11.41-订单统计
    孙振美-1.13-11.41-订单统计

--- 项目/src/views/stat/order.vue
+++ 项目/src/views/stat/order.vue
@@ -1,3 +1,53 @@
 <template>
-  <div>订单统计</div>
+  <div class="app-container">
+    <ve-line
+      :extend="chartExtend"
+      :data="chartData"
+      :settings="chartSettings"
+    />
+  </div>
 </template>
+
+<script>
+import { statOrder } from "@/api/stat";
+import VeLine from "v-charts/lib/line";
+
+export default {
+  components: { VeLine },
+  data() {
+    return {
+      chartData: {},
+      chartSettings: {},
+      chartExtend: {},
+    };
+  },
+  created() {
+    statOrder().then((response) => {
+      this.chartData = response.data.data;
+      this.chartSettings = {
+        tooltip: {
+          trigger: "item",
+        },
+        legend: {
+          data: [
+            { '订单量': orders },
+            { '下单用户': customers },
+            { '订单总额': amount },
+            { '客单价': pcr },
+          ],
+        },
+      };
+      this.chartExtend = {
+        xAxis: {
+          type: "category",
+          boundaryGap: false,
+          data: [day],
+        },
+        series: {
+          label: { show: true, position: "top" },
+        },
+      };
+    });
+  },
+};
+</script>
